78315730140043a1bbccb04f93587a9baade0d86
Fehek <fehek@outlook.com>
2021-04-18 00:06:14 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,5 @@
 module.exports = {
-  publicPath: './',
-  productionSourceMap: false,
-  configureWebpack: {
-    externals:
-      process.env.NODE_ENV === 'production'
-        ? '/vue_todos/'
-        : '/'
-  },
+  publicPath: process.env.NODE_ENV === "production" ? "/vue_todos" : "/",
   devServer: {
     overlay: {
       warnings: false,
